26b9244030c8d060023bd30e924cd5d448a627b8
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-23 12:19:57 -0400

    Primer commit

---
+++ src/main/java/com/example/thirdspringbootpostgresql/ThirdSpringbootPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdspringbootpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdSpringbootPostgresqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThirdSpringbootPostgresqlApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/thirdspringbootpostgresql/ThirdSpringbootPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thirdspringbootpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdSpringbootPostgresqlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
